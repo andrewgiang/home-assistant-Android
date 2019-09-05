208142418fb27b395c4784e683196879416f4231
Andrew Giang <andrew.giang@detroitlabs.com>
2019-09-05 11:54:10 -0400

    Show or hide wifi message based on wifi connectivity

--- app/src/main/java/io/homeassistant/android/onboarding/OnboardingFragment.kt
+++ app/src/main/java/io/homeassistant/android/onboarding/OnboardingFragment.kt
@@ -1,8 +1,20 @@
 package io.homeassistant.android.onboarding
 
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
+import androidx.core.content.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import io.homeassistant.android.R
@@ -14,10 +26,61 @@ class OnboardingFragment : Fragment(R.layout.fragment_onboarding) {
         super.onViewCreated(view, savedInstanceState)
         animationView.setMaxFrame("Circles Formed")
         animationView.playAnimation()
+
         continueBtn.setOnClickListener {
-            findNavController().navigate(OnboardingFragmentDirections.toScanningFragment())
+            if (wifiMessage.visibility != View.VISIBLE) {
+                findNavController().navigate(OnboardingFragmentDirections.toScanningFragment())
+            } else {
+                findNavController().navigate(R.id.manualConfigurationFragment)
+            }
+        }
+
+        listenForNetworkChange()
+    }
+
+    private val broadcastReceiver = object : BroadcastReceiver() {
+
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val connectivityManager = requireContext().getSystemService<ConnectivityManager>()
+            connectivityManager?.getNetworkInfo(ConnectivityManager.TYPE_WIFI)?.let { networkInfo ->
+                showWifiMessage(!networkInfo.isConnected)
+            }
+        }
+
+    }
+
+    private fun showWifiMessage(shouldShow: Boolean) {
+
+        wifiMessage.animate().alpha(if (shouldShow) 1f else 0f).withEndAction {
+            wifiMessage.visibility = if (shouldShow) View.VISIBLE else View.INVISIBLE
         }
     }
 
-    //TODO hide or show wifi warning depending on network status
+    private fun listenForNetworkChange() {
+        val connectivityManager = requireContext().getSystemService<ConnectivityManager>()
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            requireActivity().registerReceiver(
+                broadcastReceiver,
+                IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
+            )
+        } else {
+            val networkRequest = NetworkRequest.Builder()
+                .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+                .build()
+            connectivityManager?.registerNetworkCallback(
+                networkRequest,
+                object : ConnectivityManager.NetworkCallback() {
+                    override fun onAvailable(network: Network?) {
+                        super.onAvailable(network)
+                        showWifiMessage(false)
+                    }
+
+                    override fun onLost(network: Network?) {
+                        super.onLost(network)
+                        showWifiMessage(true)
+                    }
+                }, Handler(Looper.getMainLooper())
+            )
+        }
+    }
 }
